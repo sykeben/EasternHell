ed55b49079837e59b441de73a97d1cb056c844bc
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2020-09-16 09:14:55 -0400

    Undo dark reader fix (didn't work).

--- custom.js
+++ custom.js
@@ -1,6 +1,3 @@
-// Disable Dark Reader.
-DarkReader.disable();
-
 // Time override (for development).
 var timeOverride = false; var overrideHour = 0; var overrideMinute = 0; var overrideDOTW = 0;
 function doOverride(hr, min, dotw) { timeOverride = true; overrideHour = hr; overrideMinute = min; overrideDOTW = dotw }
