27c4188d98bd5fd7ee8ceec4697c54b3009cec69
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2020-09-17 14:27:17 -0400

    Added animations and fixed formatting.

--- custom.js
+++ custom.js
@@ -4,20 +4,20 @@ function doOverride(hr, min, dotw) { timeOverride = true; overrideHour = hr; ove
 function undoOverride() { timeOverride = false }
 
 // Initial setup.
-if (localStorage.getItem("group") == null) { localStorage.setItem("group", "gold") }
+if (localStorage.getItem('group') == null) { localStorage.setItem('group', 'gold') }
 var thenPeriod = -1;
 var startupDelay = true;
 
 // Load bell sound.
 var bell = new Howl({
-    src: [ "bell.ogg", "bell.mp3", "bell.wav"]
+    src: [ 'bell.ogg', 'bell.mp3', 'bell.wav']
 });
 
 // Table updater.
 function updateTable() {
 
     // Initialize current specifics.
-    var group = localStorage.getItem("group");
+    var group = localStorage.getItem('group');
     var now = new Date(); 
     var nowHour = now.getHours();
     var nowMinute = now.getMinutes();
@@ -32,26 +32,26 @@ function updateTable() {
     }
 
     // Understand block.
-    nowBlock = "break";
+    nowBlock = 'break';
     switch (nowHour) {
-        case 7: if (nowMinute >= 40) { nowBlock = "am" } break;
-        case 8: case 9: nowBlock = "am"; break;
-        case 10: if (nowMinute < 51) { nowBlock = "am" } break;
-        case 11: if (nowMinute >= 58) { nowBlock = "pm" } break;
-        case 12: case 13: nowBlock = "pm"; break;
-        case 14: if (nowMinute < 59) { nowBlock = "pm" } break;
-        default: nowBlock = "break"; break;
+        case 7: if (nowMinute >= 40) { nowBlock = 'am' } break;
+        case 8: case 9: nowBlock = 'am'; break;
+        case 10: if (nowMinute < 51) { nowBlock = 'am' } break;
+        case 11: if (nowMinute >= 58) { nowBlock = 'pm' } break;
+        case 12: case 13: nowBlock = 'pm'; break;
+        case 14: if (nowMinute < 59) { nowBlock = 'pm' } break;
+        default: nowBlock = 'break'; break;
     }
 
     // Understand day.
-    nowDay = "off";
+    nowDay = 'off';
     switch (nowDOTW) {
-        case 1: nowDay = "mon"; break;
-        case 2: nowDay = "tue"; break;
-        case 3: nowDay = "wed"; break;
-        case 4: nowDay = "thu"; break;
-        case 5: nowDay = "fri"; break;
-        default: nowDay = "off"; break;
+        case 1: nowDay = 'mon'; break;
+        case 2: nowDay = 'tue'; break;
+        case 3: nowDay = 'wed'; break;
+        case 4: nowDay = 'thu'; break;
+        case 5: nowDay = 'fri'; break;
+        default: nowDay = 'off'; break;
     }
 
     // Understand period.
@@ -68,85 +68,85 @@ function updateTable() {
         default: nowPeriod = -1; break;
     }
 
-    $(".group-gold span").removeClass("selected-hour");
-    $(".group-blue span").removeClass("selected-hour");
+    $('.group-gold span').removeClass('selected-hour');
+    $('.group-blue span').removeClass('selected-hour');
 
     // Update selections.
-    if (group == "gold") {
+    if (group == 'gold') {
 
-        $(".group-gold.h3").addClass("selected-group");
-        $(".group-blue").removeClass("selected-group selected-block selected-day");
+        $('.group-gold.h3').addClass('selected-group');
+        $('.group-blue').removeClass('selected-group selected-block selected-day');
 
-        if (nowBlock == "break") {
+        if (nowBlock == 'break') {
 
-            $(".group-gold").removeClass("selected-group selected-block selected-day");
-            for (let i=1; i<8; i++) { $(`.group-gold hour-${i}`).removeClass("selected-hour") }
+            $('.group-gold').removeClass('selected-group selected-block selected-day');
+            for (let i=1; i<8; i++) { $(`.group-gold hour-${i}`).removeClass('selected-hour') }
 
-        } else if (nowBlock == "am") {
+        } else if (nowBlock == 'am') {
 
-            $(".group-gold.time-am").addClass("selected-block");
-            $(".group-gold.time-pm").removeClass("selected-block");
+            $('.group-gold.time-am').addClass('selected-block');
+            $('.group-gold.time-pm').removeClass('selected-block');
 
-            if (nowDay != "off") {
+            if (nowDay != 'off') {
 
-                $(`.group-gold.time-am:not(day-${nowDay})`).removeClass("selected-day");
-                $(`.group-gold.time-am.day-${nowDay}`).addClass("selected-day");
+                $(`.group-gold.time-am:not(day-${nowDay})`).removeClass('selected-day');
+                $(`.group-gold.time-am.day-${nowDay}`).addClass('selected-day');
 
-                $(`.group-gold.time-${nowBlock}.day-${nowDay} span.hour-${nowPeriod}`).addClass("selected-hour");
+                $(`.group-gold.time-${nowBlock}.day-${nowDay} span.hour-${nowPeriod}`).addClass('selected-hour');
 
             }
 
-        } else if (nowBlock == "pm") {
+        } else if (nowBlock == 'pm') {
 
-            $(".group-gold.time-am").removeClass("selected-block");
-            $(".group-gold.time-pm").addClass("selected-block");
+            $('.group-gold.time-am').removeClass('selected-block');
+            $('.group-gold.time-pm').addClass('selected-block');
 
-            if (nowDay != "off") {
+            if (nowDay != 'off') {
 
-                $(`.group-gold.time-pm:not(day-${nowDay})`).removeClass("selected-day");
-                $(`.group-gold.time-pm.day-${nowDay}`).addClass("selected-day");
+                $(`.group-gold.time-pm:not(day-${nowDay})`).removeClass('selected-day');
+                $(`.group-gold.time-pm.day-${nowDay}`).addClass('selected-day');
 
-                $(`.group-gold.time-${nowBlock}.day-${nowDay} span.hour-${nowPeriod}`).addClass("selected-hour");
+                $(`.group-gold.time-${nowBlock}.day-${nowDay} span.hour-${nowPeriod}`).addClass('selected-hour');
 
             }
 
         }
 
-    } else if (group == "blue") {
+    } else if (group == 'blue') {
 
-        $(".group-blue.h3").addClass("selected-group");
-        $(".group-gold").removeClass("selected-group selected-block selected-day");
+        $('.group-blue.h3').addClass('selected-group');
+        $('.group-gold').removeClass('selected-group selected-block selected-day');
 
-        if (nowBlock == "break") {
+        if (nowBlock == 'break') {
 
-            $(".group-blue").removeClass("selected-group selected-block selected-day");
-            for (let i=1; i<8; i++) { $(`.group-blue hour-${i}`).removeClass("selected-hour") }
+            $('.group-blue').removeClass('selected-group selected-block selected-day');
+            for (let i=1; i<8; i++) { $(`.group-blue hour-${i}`).removeClass('selected-hour') }
 
-        } else if (nowBlock == "am") {
+        } else if (nowBlock == 'am') {
 
-            $(".group-blue.time-am").addClass("selected-block");
-            $(".group-blue.time-pm").removeClass("selected-block");
+            $('.group-blue.time-am').addClass('selected-block');
+            $('.group-blue.time-pm').removeClass('selected-block');
 
-            if (nowDay != "off") {
+            if (nowDay != 'off') {
 
-                $(`.group-blue.time-am:not(day-${nowDay})`).removeClass("selected-day");
-                $(`.group-blue.time-am.day-${nowDay}`).addClass("selected-day");
+                $(`.group-blue.time-am:not(day-${nowDay})`).removeClass('selected-day');
+                $(`.group-blue.time-am.day-${nowDay}`).addClass('selected-day');
 
-                $(`.group-blue.time-${nowBlock}.day-${nowDay} span.hour-${nowPeriod}`).addClass("selected-hour");
+                $(`.group-blue.time-${nowBlock}.day-${nowDay} span.hour-${nowPeriod}`).addClass('selected-hour');
 
             }
 
-        } else if (nowBlock == "pm") {
+        } else if (nowBlock == 'pm') {
 
-            $(".group-blue.time-am").removeClass("selected-block");
-            $(".group-blue.time-pm").addClass("selected-block");
+            $('.group-blue.time-am').removeClass('selected-block');
+            $('.group-blue.time-pm').addClass('selected-block');
 
-            if (nowDay != "off") {
+            if (nowDay != 'off') {
 
-                $(`.group-blue.time-pm:not(day-${nowDay})`).removeClass("selected-day");
-                $(`.group-blue.time-pm.day-${nowDay}`).addClass("selected-day");
+                $(`.group-blue.time-pm:not(day-${nowDay})`).removeClass('selected-day');
+                $(`.group-blue.time-pm.day-${nowDay}`).addClass('selected-day');
 
-                $(`.group-blue.time-${nowBlock}.day-${nowDay} span.hour-${nowPeriod}`).addClass("selected-hour");
+                $(`.group-blue.time-${nowBlock}.day-${nowDay} span.hour-${nowPeriod}`).addClass('selected-hour');
 
             }
 
@@ -155,20 +155,20 @@ function updateTable() {
     }
 
     // Update title.
-    document.title = "Eastern Hell (";
+    document.title = 'Eastern Hell (';
     switch (nowPeriod) {
-        case 1: document.title += "1st Hour"; break;
-        case 2: document.title += "2nd Hour"; break;
-        case 3: document.title += "3rd Hour"; break;
-        case 5: document.title += "5th Hour"; break;
-        case 6: document.title += "6th Hour"; break;
-        case 7: document.title += "7th Hour"; break;
-        default: document.title += "Break"; break;
+        case 1: document.title += '1st Hour'; break;
+        case 2: document.title += '2nd Hour'; break;
+        case 3: document.title += '3rd Hour'; break;
+        case 5: document.title += '5th Hour'; break;
+        case 6: document.title += '6th Hour'; break;
+        case 7: document.title += '7th Hour'; break;
+        default: document.title += 'Break'; break;
     }
-    document.title += ")";
+    document.title += ')';
 
     // Play sounds.
-    if (nowDay != "off" && thenPeriod != nowPeriod) {
+    if (nowDay != 'off' && thenPeriod != nowPeriod) {
         if (!startupDelay) { bell.play() }
         thenPeriod = nowPeriod;
     }
@@ -178,19 +178,19 @@ setInterval(updateTable, 2500);
 
 // Dropdown updater.
 function updateDropdown() {
-    var group = localStorage.getItem("group");
-    if (group == "gold") {
-        $("#group-dropdown-gold").addClass("selected");
-        $("#group-dropdown-blue").removeClass("selected");
-    } else if (group == "blue") {
-        $("#group-dropdown-gold").removeClass("selected");
-        $("#group-dropdown-blue").addClass("selected");
+    var group = localStorage.getItem('group');
+    if (group == 'gold') {
+        $('#group-dropdown-gold').addClass('selected');
+        $('#group-dropdown-blue').removeClass('selected');
+    } else if (group == 'blue') {
+        $('#group-dropdown-gold').removeClass('selected');
+        $('#group-dropdown-blue').addClass('selected');
     }
 }
 
 // Dropdown events.
-$("#group-dropdown-gold").click(() => { localStorage.setItem("group", "gold"); updateDropdown(); updateTable(); });
-$("#group-dropdown-blue").click(() => { localStorage.setItem("group", "blue"); updateDropdown(); updateTable(); });
+$('#group-dropdown-gold').click(() => { localStorage.setItem('group', 'gold'); updateDropdown(); updateTable(); });
+$('#group-dropdown-blue').click(() => { localStorage.setItem('group', 'blue'); updateDropdown(); updateTable(); });
 
 // Onload setup.
 setTimeout(function() {
@@ -204,4 +204,8 @@ setTimeout(function() {
     // Startup delay.
     setTimeout(function() { startupDelay = false }, 2500);
 
-}, 250);
+}, 125);
+
+// Fix the scrollbars.
+$('body').addClass('no-scroll');
+setTimeout(function() { $('body').removeClass('no-scroll') }, 2500);
